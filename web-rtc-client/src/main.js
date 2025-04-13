47478a330ef541854ae1e9a7b0c2ebe3c1248a60
FatherOfGloom <antonuh2002@gmail.com>
2025-04-13 20:31:43 +0300

    First working web-rtc-client version

--- web-rtc-client/src/main.js
+++ web-rtc-client/src/main.js
@@ -1,7 +1,44 @@
 import './style.css'
 
 const screenshareButton = document.getElementById('screenshareButton');
+const localVideo = document.getElementById('localVideo');
+
+const pc = new RTCPeerConnection({
+  iceServers: [
+    { urls: 'stun:stun.l.google.com:19302' }
+  ]
+});
+
+let remoteStream = null;
 
 screenshareButton.onclick = async () => {
-  console.log("LOL");
+  console.log("screenshare clicked");
+
+  const stream = await navigator.mediaDevices.getDisplayMedia({
+    video: true,
+    audio: true
+  });
+
+  remoteStream = new MediaStream();
+
+  stream.getTracks().forEach(t => pc.addTrack(t, stream));
+
+  // pc.onicecandidate = ({ candidate }) => {
+  //   if (candidate) {
+  //     // signalingSend({ type: 'ice-candidate', candidate});
+  //   }
+  // }
+
+  pc.ontrack = (event) => {
+    // localVideo.srcObject = event.streams[0];
+    event.streams[0].getTracks().forEach((track) => {
+      remoteStream.addTrack(track);
+    });
+  };
+
+  localVideo.srcObject = stream;
+
+  // const offer = await pc.createOffer();
+  // await pc.setLocalDescription(offer);
+  // signalingSend({ type: 'offer', sdp: offer.sdp });
 };
